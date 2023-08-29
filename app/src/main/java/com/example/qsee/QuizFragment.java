cbdcfa65a8ae3be00994d2ee86d337edce3bc65d
JoshEbay <joshaaron.ebay.cics@ust.edu.ph>
2023-08-29 21:17:03 +0800

    Added Quiz Page (no functions)

--- app/src/main/java/com/example/qsee/QuizFragment.java
+++ app/src/main/java/com/example/qsee/QuizFragment.java
@@ -1,10 +1,14 @@
 package com.example.qsee;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class QuizFragment extends Fragment {
@@ -13,9 +17,43 @@ public class QuizFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quiz, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_quiz, container, false);
+        Button btA = rootView.findViewById(R.id.btA);
+        Button btB = rootView.findViewById(R.id.btB);
+        Button btC = rootView.findViewById(R.id.btC);
+        Button btD = rootView.findViewById(R.id.btD);
+
+        btA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle button click event
+                // You can add any desired behavior here
+            }
+        });
+        btB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle button click event
+                // You can add any desired behavior here
+            }
+        });
+        btC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle button click event
+                // You can add any desired behavior here
+            }
+        });
+        btD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle button click event
+                // You can add any desired behavior here
+            }
+        });
+
+        return rootView;
     }
 }
