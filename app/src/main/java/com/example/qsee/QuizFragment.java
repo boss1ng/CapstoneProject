96a3fe2a72f55fdd9ac7edb1ce22c2792631baba
JoshEbay <joshaaron.ebay.cics@ust.edu.ph>
2023-08-30 21:03:28 +0800

    Added colors to buttons and some if statements

--- app/src/main/java/com/example/qsee/QuizFragment.java
+++ app/src/main/java/com/example/qsee/QuizFragment.java
@@ -1,6 +1,5 @@
 package com.example.qsee;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,20 +9,20 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.content.ContextCompat;
 import androidx.core.view.ViewCompat;
 import androidx.fragment.app.Fragment;
 
-import org.w3c.dom.Text;
-
 public class QuizFragment extends Fragment {
-    public QuizFragment() {
-        // Required empty public constructor
-    }
+    // ... (other methods and variables)
+
+    private Button correctButton; // Reference to the correct button
+    private boolean isAnswerCorrect = false;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        // ... (other code)
         View rootView = inflater.inflate(R.layout.fragment_quiz, container, false);
         Button btA = rootView.findViewById(R.id.btA);
         Button btB = rootView.findViewById(R.id.btB);
@@ -34,52 +33,61 @@ public class QuizFragment extends Fragment {
         TextView Q2 = rootView.findViewById(R.id.Q2);
         TextView Q3 = rootView.findViewById(R.id.Q3);
         TextView Q4 = rootView.findViewById(R.id.Q4);
+        // Set the correct button
+        correctButton = btA;
 
-        btA.setOnClickListener(new View.OnClickListener() {
+        View.OnClickListener buttonClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Handle button click event
-                // You can add any desired behavior here
+                if (isAnswerCorrect) {
+                    return; // If the answer is already correct, do nothing
+                }
+
                 Button clickedButton = (Button) v;
-                clickedButton.setBackgroundResource(R.drawable.button_gradient);
-                ViewCompat.setElevation(clickedButton, getResources().getDimensionPixelSize(R.dimen.button_elevation));
-                ViewCompat.setElevation(Q1, getResources().getDimensionPixelSize(R.dimen.textview_elevation));
+                if (clickedButton == correctButton) {
+                    handleCorrectAnswerButtonClick(clickedButton, Q1);
+                } else {
+                    handleWrongAnswerButtonClick(clickedButton);
+                }
             }
-        });
-        btB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Handle button click event
-                // You can add any desired behavior here
-                Button clickedButton = (Button) v;
-                clickedButton.setBackgroundTintList(ContextCompat.getColorStateList(requireContext(), R.color.gradient_start));
-                ViewCompat.setElevation(clickedButton, getResources().getDimensionPixelSize(R.dimen.button_elevation));
-                ViewCompat.setElevation(Q2, getResources().getDimensionPixelSize(R.dimen.textview_elevation));
-            }
-        });
-        btC.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Handle button click event
-                // You can add any desired behavior here
-                Button clickedButton = (Button) v;
-                clickedButton.setBackgroundTintList(ContextCompat.getColorStateList(requireContext(), R.color.gradient_start));
-                ViewCompat.setElevation(clickedButton, getResources().getDimensionPixelSize(R.dimen.button_elevation));
-                ViewCompat.setElevation(Q3, getResources().getDimensionPixelSize(R.dimen.textview_elevation));
-            }
-        });
-        btD.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Handle button click event
-                // You can add any desired behavior here
-                Button clickedButton = (Button) v;
-                clickedButton.setBackgroundTintList(ContextCompat.getColorStateList(requireContext(), R.color.gradient_start));
-                ViewCompat.setElevation(clickedButton, getResources().getDimensionPixelSize(R.dimen.button_elevation));
-                ViewCompat.setElevation(Q4, getResources().getDimensionPixelSize(R.dimen.textview_elevation));
-            }
-        });
+        };
+
+        btA.setOnClickListener(buttonClickListener);
+        btB.setOnClickListener(buttonClickListener);
+        btC.setOnClickListener(buttonClickListener);
+        btD.setOnClickListener(buttonClickListener);
 
         return rootView;
+    }
+
+    private void handleCorrectAnswerButtonClick(Button clickedButton, TextView questionTextView) {
+        clickedButton.setBackgroundTintList(ContextCompat.getColorStateList(requireContext(), R.color.gradient_start));
+        ViewCompat.setElevation(clickedButton, getResources().getDimensionPixelSize(R.dimen.button_elevation));
+        ViewCompat.setElevation(questionTextView, getResources().getDimensionPixelSize(R.dimen.textview_elevation));
+
+        isAnswerCorrect = true;
+        showCorrectAnswerDialog();
+    }
+
+    private void handleWrongAnswerButtonClick(Button clickedButton) {
+        clickedButton.setBackgroundTintList(ContextCompat.getColorStateList(requireContext(), R.color.gradient_end));
+        showWrongAnswerDialog();
+    }
+
+    private void showCorrectAnswerDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        builder.setTitle("Correct Answer")
+                .setMessage("Your answer is correct!")
+                .setPositiveButton("Proceed", null)
+                .setNegativeButton("Cancel", null)
+                .show();
+    }
+
+    private void showWrongAnswerDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        builder.setTitle("Wrong Answer")
+                .setMessage("Try again!")
+                .setPositiveButton("OK", null)
+                .show();
     }
 }
