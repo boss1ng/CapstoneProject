2b5ac0f6467c9791835570b4b5cd7bbca3706ced
JoshEbay <joshaaron.ebay.cics@ust.edu.ph>
2023-08-30 18:34:42 +0800

    Added Button functions in Quiz Tab (button elevation once clicked, colors still at progress)

--- app/src/main/java/com/example/qsee/QuizFragment.java
+++ app/src/main/java/com/example/qsee/QuizFragment.java
@@ -6,10 +6,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.core.view.ViewCompat;
 import androidx.fragment.app.Fragment;
+
+import org.w3c.dom.Text;
 
 public class QuizFragment extends Fragment {
     public QuizFragment() {
@@ -25,11 +30,20 @@ public class QuizFragment extends Fragment {
         Button btC = rootView.findViewById(R.id.btC);
         Button btD = rootView.findViewById(R.id.btD);
 
+        TextView Q1 = rootView.findViewById(R.id.Q1);
+        TextView Q2 = rootView.findViewById(R.id.Q2);
+        TextView Q3 = rootView.findViewById(R.id.Q3);
+        TextView Q4 = rootView.findViewById(R.id.Q4);
+
         btA.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Handle button click event
                 // You can add any desired behavior here
+                Button clickedButton = (Button) v;
+                clickedButton.setBackgroundResource(R.drawable.button_gradient);
+                ViewCompat.setElevation(clickedButton, getResources().getDimensionPixelSize(R.dimen.button_elevation));
+                ViewCompat.setElevation(Q1, getResources().getDimensionPixelSize(R.dimen.textview_elevation));
             }
         });
         btB.setOnClickListener(new View.OnClickListener() {
@@ -37,6 +51,10 @@ public class QuizFragment extends Fragment {
             public void onClick(View v) {
                 // Handle button click event
                 // You can add any desired behavior here
+                Button clickedButton = (Button) v;
+                clickedButton.setBackgroundTintList(ContextCompat.getColorStateList(requireContext(), R.color.gradient_start));
+                ViewCompat.setElevation(clickedButton, getResources().getDimensionPixelSize(R.dimen.button_elevation));
+                ViewCompat.setElevation(Q2, getResources().getDimensionPixelSize(R.dimen.textview_elevation));
             }
         });
         btC.setOnClickListener(new View.OnClickListener() {
@@ -44,6 +62,10 @@ public class QuizFragment extends Fragment {
             public void onClick(View v) {
                 // Handle button click event
                 // You can add any desired behavior here
+                Button clickedButton = (Button) v;
+                clickedButton.setBackgroundTintList(ContextCompat.getColorStateList(requireContext(), R.color.gradient_start));
+                ViewCompat.setElevation(clickedButton, getResources().getDimensionPixelSize(R.dimen.button_elevation));
+                ViewCompat.setElevation(Q3, getResources().getDimensionPixelSize(R.dimen.textview_elevation));
             }
         });
         btD.setOnClickListener(new View.OnClickListener() {
@@ -51,6 +73,10 @@ public class QuizFragment extends Fragment {
             public void onClick(View v) {
                 // Handle button click event
                 // You can add any desired behavior here
+                Button clickedButton = (Button) v;
+                clickedButton.setBackgroundTintList(ContextCompat.getColorStateList(requireContext(), R.color.gradient_start));
+                ViewCompat.setElevation(clickedButton, getResources().getDimensionPixelSize(R.dimen.button_elevation));
+                ViewCompat.setElevation(Q4, getResources().getDimensionPixelSize(R.dimen.textview_elevation));
             }
         });
 
