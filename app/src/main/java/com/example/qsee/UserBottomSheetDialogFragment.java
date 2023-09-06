fb8a6942a9963c8edd9debe766440a0345ee433c
euneez <euneezayessa.lara.cics@ust.edu.ph>
2023-09-06 11:44:11 +0800

    Additional User Options Update

--- app/src/main/java/com/example/qsee/UserBottomSheetDialogFragment.java
+++ app/src/main/java/com/example/qsee/UserBottomSheetDialogFragment.java
@@ -22,24 +22,9 @@ public class UserBottomSheetDialogFragment extends BottomSheetDialogFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_user_options_bottom_sheet, container, false);
 
-        TextView registeredLocationsOption = rootView.findViewById(R.id.menu_registered_locations);
-        TextView registerLocationOption = rootView.findViewById(R.id.menu_register_location);
         TextView changePasswordOption = rootView.findViewById(R.id.menu_change_password);
         TextView signOutOption = rootView.findViewById(R.id.menu_sign_out);
 
-        registeredLocationsOption.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Handle Registered Locations option click
-            }
-        });
-
-        registerLocationOption.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Handle Register a Location option click
-            }
-        });
 
         changePasswordOption.setOnClickListener(new View.OnClickListener() {
             @Override
