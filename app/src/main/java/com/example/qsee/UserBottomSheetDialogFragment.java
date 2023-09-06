ce85bdc09d8af860602cffe69d33f4ca246cb2ee
euneez <euneezayessa.lara.cics@ust.edu.ph>
2023-09-06 12:02:46 +0800

    Additional User Options Updated

--- app/src/main/java/com/example/qsee/UserBottomSheetDialogFragment.java
+++ app/src/main/java/com/example/qsee/UserBottomSheetDialogFragment.java
@@ -31,7 +31,6 @@ public class UserBottomSheetDialogFragment extends BottomSheetDialogFragment {
                 // Handle Change Password option click
             }
         });
-
         signOutOption.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
