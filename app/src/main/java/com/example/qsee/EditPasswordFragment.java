d2db0f3e9cb45f137b3d31c4fdf6e72c1e9c8cec
JoshEbay <joshaaron.ebay.cics@ust.edu.ph>
2023-09-13 21:05:30 +0800

    Edit Password fix onclick and edittext

--- app/src/main/java/com/example/qsee/EditPasswordFragment.java
+++ app/src/main/java/com/example/qsee/EditPasswordFragment.java
@@ -1,6 +1,31 @@
 package com.example.qsee;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 public class EditPasswordFragment extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_editpassword, container, false);
+        Button cancel = view.findViewById(R.id.cancelBt);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getParentFragmentManager();
+                fragmentManager.popBackStack();
+            }
+        });
+
+        return view;
+    }
+
 }
+
--- app/src/main/java/com/example/qsee/UserBottomSheetDialogFragment.java
+++ app/src/main/java/com/example/qsee/UserBottomSheetDialogFragment.java
@@ -1,19 +1,17 @@
 package com.example.qsee;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentTransaction;
-
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
-public class    UserBottomSheetDialogFragment extends BottomSheetDialogFragment {
+public class UserBottomSheetDialogFragment extends BottomSheetDialogFragment {
 
     public UserBottomSheetDialogFragment() {
         // Required empty public constructor
@@ -26,33 +24,30 @@ public class    UserBottomSheetDialogFragment extends BottomSheetDialogFragment 
 
         TextView changePasswordOption = rootView.findViewById(R.id.menu_change_password);
         TextView signOutOption = rootView.findViewById(R.id.menu_sign_out);
-        changePasswordOption.setOnClickListener(new View.OnClickListener() {
-               public void onClick(View v) {
-                   // Replace the current fragment with the "fragment_quiz" fragment
-                   EditPasswordFragment editPasswordFragment = new EditPasswordFragment();
-                   FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
-                   transaction.replace(R.id.fragment_container, editPasswordFragment); // Replace 'fragment_container' with the ID of your container layout
-                   transaction.addToBackStack(null); // Optional: Add the transaction to the back stack
-                   transaction.commit();
-               }
-           });
-
 
         changePasswordOption.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Handle Change Password option click
+                // Replace the current fragment with the "EditPasswordFragment"
+                EditPasswordFragment editPasswordFragment = new EditPasswordFragment();
+                FragmentTransaction transaction = requireActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, editPasswordFragment); // Replace 'fragment_container' with the ID of your container layout
+                transaction.addToBackStack(null); // Optional: Add the transaction to the back stack
+                transaction.commit();
+                dismiss(); // Close the bottom sheet dialog after the fragment is replaced
             }
         });
+
         signOutOption.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Handle Sign Out option click
                 Intent intent = new Intent(getContext(), MainActivity.class);
                 startActivity(intent);
+                dismiss(); // Close the bottom sheet dialog after starting the new activity
             }
         });
+
         return rootView;
     }
-
 }
