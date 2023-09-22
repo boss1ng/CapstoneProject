cdd288a621db532ab3effcdbcc62f254786a36c7
bry <122538578+boss1ng@users.noreply.github.com>
2023-09-22 21:12:54 +0800

    Added Android Maps SDK and Constrained to QC

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,13 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Google Maps SDK
+    implementation("com.google.android.gms:play-services-maps:17.0.1")
+
+    implementation("androidx.fragment:fragment:1.5.0")
+
+    // implementation("com.google.android.gms:play-services-maps:17.0.0")
+    // implementation("com.google.android.gms:play-services-location:18.0.0") // If you need location services
+
 }
--- app/src/main/java/com/example/qsee/MapsFragment.java
+++ app/src/main/java/com/example/qsee/MapsFragment.java
@@ -5,18 +5,94 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-public class MapsFragment extends Fragment {
+// Import
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+// Quezon City ONLY
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+
+
+public class MapsFragment extends Fragment implements OnMapReadyCallback {
+
+    private MapView mapView;
+    private GoogleMap mMap;
 
     public MapsFragment() {
         // Required empty public constructor
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_maps, container, false);
+        //return inflater.inflate(R.layout.fragment_maps, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_maps, container, false);
+
+        // Obtain the FragmentManager using getParentFragmentManager()
+        FragmentManager fragmentManager = getParentFragmentManager();
+
+        // Use fragmentManager to work with fragments, including SupportMapFragment
+        SupportMapFragment mapFragment = new SupportMapFragment();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.add(R.id.maps, mapFragment).commit();
+
+        // SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager.findFragmentId(R.id.maps);
+        mapFragment.getMapAsync(this);
+
+        return view;
     }
+
+//    @Override
+//    public void onMapReady(@NonNull GoogleMap googleMap) {
+//
+//    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Define the bounds for Quezon City, Philippines
+        LatLngBounds quezonCityBounds = new LatLngBounds(
+                new LatLng(14.6138, 121.0357), // Southwest corner
+                new LatLng(14.7395, 121.0711)  // Northeast corner
+        );
+
+        // Constrain the map's camera position to Quezon City bounds
+        mMap.setLatLngBoundsForCameraTarget(quezonCityBounds);
+
+        // Move the camera to Quezon City
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(quezonCityBounds.getCenter(), 12));
+    }
+
+    /*
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+*/
 }
