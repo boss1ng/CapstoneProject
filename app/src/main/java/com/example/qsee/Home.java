7806d8f9aa6351ad555fce862ff4f5983ffddd18
Joshua Asiddao <joshuadaniel.asiddao.cics@ust.edu.ph>
2023-11-22 12:31:57 +0800

    Avoided Duplication of Home NavBar

--- app/src/main/java/com/example/qsee/Home.java
+++ app/src/main/java/com/example/qsee/Home.java
@@ -24,33 +24,6 @@ public class Home extends AppCompatActivity {
 
         String userId = getIntent().getStringExtra("userId");
 
-        bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        // Set the default item as highlighted
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int itemId = item.getItemId();
-                if (itemId == R.id.action_home) {
-                    loadFragment(new HomeFragment(), userId);
-                    bottomNavigationView.setVisibility(View.GONE);
-                } else if (itemId == R.id.action_search) {
-                    loadFragment(new SearchFragment(), userId);
-                    bottomNavigationView.setVisibility(View.GONE);
-                } else if (itemId == R.id.action_maps) {
-                    loadFragment(new MapsFragment(), userId);
-                    //BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
-                    bottomNavigationView.setVisibility(View.GONE);
-                } else if (itemId == R.id.action_quiz) {
-                    loadFragment(new StartQuizFragment(), userId);
-                    bottomNavigationView.setVisibility(View.GONE);
-                } else if (itemId == R.id.action_profile) {
-                    loadFragment(new ProfileFragment(), userId);
-                    bottomNavigationView.setVisibility(View.GONE);
-                }
-                return true;
-            }
-        });
-
         // Load HomeFragment when the activity starts
         loadFragment(new HomeFragment(), userId);
     }
